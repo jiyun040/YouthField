66fc273bcba098217953d12ed7d6e4049a6ef87e
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-15 23:08:05 +0900

    feat: title 컴포넌트 추가

---
+++ lib/core/widgets/yf_title_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:youthfield/core/constants/color.dart';
+import 'package:youthfield/core/constants/text_style.dart';
+
+class YFTitleBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showBack;
+  final VoidCallback? onBack;
+  final List<Widget> actions;
+
+  const YFTitleBar({
+    super.key,
+    required this.title,
+    this.showBack = true,
+    this.onBack,
+    this.actions = const [],
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(64);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64,
+      color: YouthFieldColor.white,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            title,
+            style: YouthFieldTextStyle.body3.copyWith(
+              color: YouthFieldColor.black800,
+            ),
+          ),
+
+          if (showBack)
+            Positioned(
+              left: 0,
+              child: IconButton(
+                onPressed: onBack ?? () => Navigator.maybePop(context),
+                icon: const Icon(
+                  Icons.chevron_left,
+                  color: YouthFieldColor.blue700,
+                  size: 32,
+                ),
+              ),
+            ),
+
+          if (actions.isNotEmpty)
+            Positioned(
+              right: 0,
+              child: Row(mainAxisSize: MainAxisSize.min, children: actions),
+            ),
+        ],
+      ),
+    );
+  }
+}
