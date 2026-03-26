a53c37c50bcfdd4d2d073364db848b5142fd784f
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-26 11:06:26 +0900

    feat: dio 추가

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio(BaseOptions(
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+  ));
+  return dio;
+});
